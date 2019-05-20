5006b6bd4b9cd7bf4cd1882814baebdb9c01d712
unknown <laxmanmanandhar7@gmail.com>
2019-05-20 10:26:48 +0545

    Postgres JSONB database setup and table initialization

--- src/main/java/com/possible/imisconnect/dao/CustomerRepository.java
+++ src/main/java/com/possible/imisconnect/dao/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.possible.imisconnect.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.possible.imisconnect.fhir.model.Customer;
